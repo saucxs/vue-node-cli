551632377e136bb5f219d724269f560221ef8bac
saucxs <saucxs@163.com>
2019-09-11 14:26:58 +0800

    init project

---
+++ bin/saucxs.js
@@ -0,0 +1,9 @@
+const program = require('commander');
+const version = require('../package.json').version;
+
+
+program
+    .version(version)
+    .command('init', 'init a structure-admin template')
+    .command('upload','upload upyun')
+    .parse(process.argv);
